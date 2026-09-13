3e63591abe90a19e63b64256eacf8212acae6c71
Aaron031002 <wang.aaron.921002@gmail.com>
2026-09-13 14:57:57 +0000

    add comment

--- ex11/main.c
+++ ex11/main.c
@@ -1,9 +1,9 @@
 #define UART_BASE 0x10000000UL
-#define UART_RBR  (unsigned char*)(UART_BASE + 0x0)
-#define UART_THR  (unsigned char*)(UART_BASE + 0x0)
+#define UART_RBR  (unsigned char*)(UART_BASE + 0x0) // Receive Buffer Register (buffer that receive data)
+#define UART_THR  (unsigned char*)(UART_BASE + 0x0) // Transmit Holding Register (holds the data to be transmitted)
 #define UART_LSR  (unsigned char*)(UART_BASE + 0x5)
-#define LSR_DR    (1 << 0)
-#define LSR_TDRQ  (1 << 5)
+#define LSR_DR    (1 << 0)  // if data is available in Receive Buffer Register
+#define LSR_TDRQ  (1 << 5)  // if UART is ready to accept a new char for transmission
 
 char uart_getc() {
     // TODO: Implement this function
