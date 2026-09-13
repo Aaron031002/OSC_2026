96c9e07f159cf3110bd37573fdc1d55f6bbc57cf
Aaron031002 <wang.aaron.921002@gmail.com>
2026-09-13 14:40:48 +0000

    completed

--- ex11/main.c
+++ ex11/main.c
@@ -7,14 +7,24 @@
 
 char uart_getc() {
     // TODO: Implement this function
+    while ((*UART_LSR) & LSR_DR == 0);
+
+    return *UART_RBR;
 }
 
 void uart_putc(char c) {
     // TODO: Implement this function
+    while ((*UART_LSR) & LSR_TDRQ == 0);
+
+    *UART_THR = c;
 }
 
 void uart_puts(const char* s) {
     // TODO: Implement this function
+    while (*s != '\0'){
+        uart_putc(*s);
+        s++;
+    }
 }
 
 void start_kernel() {
