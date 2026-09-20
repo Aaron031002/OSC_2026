9b56e1b27c5a063bdd66b3f88083000bd32e0231
Aaron031002 <wang.aaron.921002@gmail.com>
2026-09-20 14:49:34 +0000

    add working flow

--- Lab2_UART-bootloader/send_kernel.py
+++ Lab2_UART-bootloader/send_kernel.py
@@ -5,15 +5,17 @@ BOOT_MAGIC = 0x544F4F42
 
 if len(sys.argv) != 3:
     print("Usage:")
-    print("python3 send_kernel.py <tty> <kernel>")
+    print("python3 send_kernel.py <tty> <kernel>")  # tty: /dev/pts/3, kernel: kernel (raw binary)
     sys.exit(1) # end the program
 
 tty_path = sys.argv[1]
 kernel_path = sys.argv[2]
 
+# prepare kernel image content
 with open(kernel_path, "rb") as f:  # binary read mode
     kernel_data = f.read()
 
+# prepare header content
 header = struct.pack("<II", BOOT_MAGIC, len(kernel_data))
 
 # write /dev/pts/3 will help user send data to UART
